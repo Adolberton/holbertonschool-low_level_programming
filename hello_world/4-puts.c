fedaa7ef0e125818caa39c9995c2d7e68e6f6459
Adolberton <7557@holbertonstudents.com>
2023-10-12 05:02:46 -0700

    auto commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	retun (0);
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
