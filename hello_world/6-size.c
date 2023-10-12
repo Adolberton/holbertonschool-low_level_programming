51440e80699f63f64a164262dbb2aeda20fe6262
Adolberton <7557@holbertonstudents.com>
2023-10-12 06:24:09 -0700

    auto commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() { 
+	printf("Taille d'un char : %lu octets\n", sizeof(char));
+	printf("Taille d'un int : %lu octets\n", sizeof(int));
+	printf("Taille d'un float : %lu octets\n", sizeof(float));
+	printf("Taille d'un double : %lu octets\n", sizeof(double));
+	printf("Taille d'un long : %lu octets\n", sizeof(long));
+	printf("Taille d'un long long : %lu octets\n", sizeof(long long));
+	
+	return 0;
+}
