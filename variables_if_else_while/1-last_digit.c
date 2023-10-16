0302e318e8d9a6edfa77541967cd5bb08d86e1ef
Adolberton <7557@holbertonstudents.com>
2023-10-16 03:39:44 -0700

    auto commit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *  Return: Always 0 (Success)
+ *  Description: ADO PD
+ */
+int main(void)
+{
+	int n, modulo;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	modulo = n % 10;
+	if (modulo > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5", n, modulo);
+	}
+	if (modulo == 0)
+	{
+	printf("Last digit of %d is %d and is 0", n, modulo);
+	}
+	if (modulo < 6 && modulo != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0", n, modulo);
+	}
+	return (0);
+}
