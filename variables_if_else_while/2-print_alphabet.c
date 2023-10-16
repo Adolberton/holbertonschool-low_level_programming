01b5a54b3979c7d9353c97a59c7e2325b18ea523
Adolberton <7557@holbertonstudents.com>
2023-10-16 04:22:40 -0700

    auto commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: sdfqdsqsdf
+ *
+ */
+main(void)
+{
+	char c;
+{
+	for (c = "a"; c <= "z"; c++)
+	putchar (c);
+}
+{
+	putchar("\d");
+	return (0);
+}
+}
