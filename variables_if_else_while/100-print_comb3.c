e1876386a5e63f5c2d4d18869b5699b8b5784d17
Adolberton <7557@holbertonstudents.com>
2023-10-16 08:37:07 -0700

    auto commit

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 	for (int a = 0 ; a <= 9 ; a++)
-	{
-		for (int b = a + 1 ; b <= 9 ; b++)
-		{
-			int end = (a * 10 + b);
-			putchar('0' + a);
-			putchar('0' + b);
-			if (end < 89)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+{
+	for (int b = a + 1 ; b <= 9 ; b++)
+{
+	int end = (a * 10 + b);
+	putchar('0' + a);
+	putchar('0' + b);
+	if (end < 89)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
 	return (0);
 }
