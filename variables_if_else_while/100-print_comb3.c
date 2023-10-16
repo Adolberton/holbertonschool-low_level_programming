8625a71569a3f05f2611fa72aa56209ef69f7d5e
Adolberton <7557@holbertonstudents.com>
2023-10-16 08:34:19 -0700

    auto commit

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: Tema le Génie
+ *
+ */
+int main(void)
+{
+	for (int a = 0 ; a <= 9 ; a++)
+	{
+		for (int b = a + 1 ; b <= 9 ; b++)
+		{
+			int end = (a * 10 + b);
+			putchar('0' + a);
+			putchar('0' + b);
+			if (end < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
