a4bccc46d87e7ad4489b99636897459f4fe82e7d
Adolberton <7557@holbertonstudents.com>
2023-10-17 01:40:47 -0700

    auto commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+
+	_putchar(c);
+	putchar('\n');
+	return (0);
+}
