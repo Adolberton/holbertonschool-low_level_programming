ea5eabe4b2b649e09824d547ddca009d78dd922e
Adolberton <7557@holbertonstudents.com>
2023-10-17 04:38:15 -0700

    auto commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Write a program that prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i < 123; i++)
+	putchar (i);
+	return (0);
+}
