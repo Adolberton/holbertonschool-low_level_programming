1b1d6a8c71f12c0fe5d5eba6e68a4725685cb2d4
Adolberton <7557@holbertonstudents.com>
2023-10-17 06:46:45 -0700

    auto commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ * print_alphabet_x10: Prototype
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
+}
