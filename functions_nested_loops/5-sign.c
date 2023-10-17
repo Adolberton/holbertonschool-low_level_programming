50274b843c57437148a1d47c5876bdcb17c1cd2f
Adolberton <7557@holbertonstudents.com>
2023-10-17 07:40:01 -0700

    auto commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,23 +5,24 @@
 *@n: the character
 *Return: 1 if the lower case is found
 */
-
 int print_sign(int n)
 {
 {
 	if (n > 0)
-	return (1);
 	_putchar ('+');
+	return (1);
+
 }
 {
 	if (n == 0)
-	return (0);
-	_putchar ('0');
-}
+		_putchar ('0');
+		return (0);
 {
 	if (n < 0)
-	return (-1);
 	_putchar ('-');
+	return (-1);
+
 }
 	return (0);
+}
 }
