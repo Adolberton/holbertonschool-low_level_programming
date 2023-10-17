9ab26e992039eb9a32473a595ffeeca891259e5d
Adolberton <7557@holbertonstudents.com>
2023-10-17 07:43:02 -0700

    auto commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
-*print_sign-check if a character is lower case
-*@n: the character
-*Return: 1 if the lower case is found
+* print_sign - returns 1 if character is lowercase else 0
+* @n: The character to be checked.
+* Return: Always 0 (Success)
 */
+
 int print_sign(int n)
 {
-{
 	if (n > 0)
-	_putchar ('+');
+{
+	_putchar('+');
 	return (1);
-
 }
-{
 	if (n == 0)
-		_putchar ('0');
-		return (0);
 {
-	if (n < 0)
-	_putchar ('-');
-	return (-1);
-
-}
+	_putchar('0');
 	return (0);
 }
+	if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+	return (0);
 }
