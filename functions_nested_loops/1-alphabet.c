cb6d932e4c962de213e2c3608da0c087d53cd6cc
Adolberton <7557@holbertonstudents.com>
2023-10-17 04:53:41 -0700

    auto commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include "main.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-i		print_alphabet();
-			return (0);
-}
-int main(void)
-{
-	int i;
-	for (i = 97; i < 123; i++)
-	putchar (i);
-	return (0);
-}
+  /**
+   * main - Entry point
+   * Description: Write a program that prints _putchar
+   * Return: Always 0 (Success)
+   */
+  int main(void)
+  {
+          int i;
+          for (i = 97; i < 123; i++)
+          putchar (i);
+          return (0);
+  }
