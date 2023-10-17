5c990f3b2b1a7504980547d2d06986ca8012704c
Adolberton <7557@holbertonstudents.com>
2023-10-17 04:45:50 -0700

    auto commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - Entry point
- * Description: Write a program that prints _putchar
- * Return: Always 0 (Success)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
+int main(void)
+{
+i		print_alphabet();
+			return (0);
+}
 int main(void)
 {
 	int i;
