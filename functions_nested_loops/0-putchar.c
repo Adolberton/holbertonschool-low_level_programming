8a3f9b9a2fc04821c963404a4c23e12b382f0e5e
Adolberton <7557@holbertonstudents.com>
2023-10-17 04:04:11 -0700

    auto commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/**main - Entry point
+/**
+ * main - Entry point
  * Description: Write a program that prints _putchar
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char str[] = "_putchar";
