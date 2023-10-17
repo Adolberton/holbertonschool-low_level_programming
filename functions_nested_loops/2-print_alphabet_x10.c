7cd7d39b42b6e4d23bd5c396e0b198e9abf7154e
Adolberton <7557@holbertonstudents.com>
2023-10-17 05:52:52 -0700

    auto commit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ * print_alphabet_x10: Prototype
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+	for (a = 0; a <= 9; a++)
+{
+
+	for (b = 'a'; b <= 'z'; b++)
+{
+	_putchar (b);
+}
+}
+}
