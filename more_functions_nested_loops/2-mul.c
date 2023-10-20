3ac1a55dfaf34f66ad358564e44332ca147157a2
Adolberton <7557@holbertonstudents.com>
2023-10-20 02:05:41 -0700

    auto commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - returns 1 if character is lowercase else 0
+* @b: The character to be checked.
+* @a: encor une desciption useless
+* Return: 1 if success, else 0.
+*/
+
+int mul(int a, int b)
+{	int c = a * b;
+
+	return (c);
+}
