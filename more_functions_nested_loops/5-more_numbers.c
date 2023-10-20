4bf0c0b0c090d3d8084f2f01c7040783899bccf8
Adolberton <7557@holbertonstudents.com>
2023-10-20 02:46:24 -0700

    auto commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* print_numbers - returns 1 if character is lowercase else 0
-* Return: 1 if success, else 0.
-*/
+ * print_numbers - returns 1 if character is lowercase else 0
+ * Return: 1 if success, else 0.
+ */
 
 void more_numbers(void)
 {
@@ -12,9 +12,9 @@ void more_numbers(void)
 	for (t = 0; t < 10; t++)
 	{	_putchar('\n');
 		for (i = 0; i < 15; i++)
-	{
-	_putchar('0' + i);
-	}
+		{
+			_putchar('0' + i);
+		}
 	}
 	_putchar('\n');
 }
