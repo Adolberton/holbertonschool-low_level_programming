cabd763958ee80d2d41326850f554183176749c9
Adolberton <7557@holbertonstudents.com>
2023-10-20 04:53:58 -0700

    auto commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - returns 1 if character is lowercase else 0
+* @n: edfsgsdf
+* Return: 1 if success, else 0.
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
