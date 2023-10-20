187be2df3777189443f5d97ec0aafbbd56489df3
Adolberton <7557@holbertonstudents.com>
2023-10-20 07:54:20 -0700

    auto commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,27 +10,21 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
-		}
-		if (i % 3 != 0 && i % 5 != 0)
+		} else if (i % 3 == 0)
 		{
-			if (i == 100)
-			{
-				printf("Buzz");
-			}
+			printf("Fizz ");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		} else
+		{
 			printf("%d ", i);
 		}
-
 	}
-return (0);
+
+	printf("\n");
+	return (0);
 }
