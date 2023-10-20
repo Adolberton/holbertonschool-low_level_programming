23ecf4b73ad6b58811605a95ff904b4bc4e2b011
Adolberton <7557@holbertonstudents.com>
2023-10-20 02:31:32 -0700

    auto commit

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-* print_numbers - returns 1 if character is lowercase else 0
+* print_most_numbers - returns 1 if character is lowercase else 0
 * Return: 1 if success, else 0.
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-	if (i != 2 && i !=4)
+	if (i != 2 && i != 4)
 	_putchar('0' + i);
 	}
 	_putchar('\n');
