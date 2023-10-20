e463cdcfed5aae7f62d1295a796992225a234216
Adolberton <7557@holbertonstudents.com>
2023-10-20 02:44:06 -0700

    auto commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,12 +5,12 @@
 * Return: 1 if success, else 0.
 */
 
-void print_numbers(void)
+void more_numbers(void)
 {
 	int i, t;
 
 	for (t = 0; t < 10; t++)
-	{
+	{	_putchar('\n');
 		for (i = 0; i < 15; i++)
 	{
 	_putchar('0' + i);
