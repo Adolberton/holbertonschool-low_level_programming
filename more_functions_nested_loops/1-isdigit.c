7ebce764d2fd753891dd2f4d541cf3e9ae967709
Adolberton <7557@holbertonstudents.com>
2023-10-20 01:48:56 -0700

    auto commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - returns 1 if character is lowercase else 0
+* @c: The character to be checked.
+* Return: 1 if success, else 0.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
