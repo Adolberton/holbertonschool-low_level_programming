ee8716d542d77386df314ba0f4b652bf4fdc81cf
Adolberton <7557@holbertonstudents.com>
2023-10-23 05:08:46 -0700

    auto commit

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - sdf
+ * Return: sqdfqsd
+ * @str: sqdqfsqf
+ */
+
+void _puts(char *str)
+{
+	int put = 0;
+
+	while (str[put] != '\0')
+		_putchar(str[put]);
+		put++;
+}
+
