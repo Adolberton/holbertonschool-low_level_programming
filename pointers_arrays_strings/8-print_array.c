314e7d1533c3fc191a9b32a9657b7560d2a53296
Adolberton <7557@holbertonstudents.com>
2023-10-24 02:08:44 -0700

    auto commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_array - Prints a string in reverse
+ * @a: The string to print
+ * @n: sqdjfq
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	n = 0;
+	while a[n] != '\0'
+	{
+		printf("%d", a[n]);
+		n++;
+	}
+}
