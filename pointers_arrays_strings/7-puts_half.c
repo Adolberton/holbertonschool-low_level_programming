e56028858c5722e95a7efe226efaf43e0bd6940b
Adolberton <7557@holbertonstudents.com>
2023-10-24 00:54:08 -0700

    auto commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints a string in reverse
+ * @str: The string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+	if (a % 2 != 0)
+		a -= 1;
+	a /= 2;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
