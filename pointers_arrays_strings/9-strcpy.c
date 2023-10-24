a87b9e7bff62c0d300b712a33324e1336544f180
Adolberton <7557@holbertonstudents.com>
2023-10-24 03:56:35 -0700

    auto commit

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,12 @@
 #include <stdio.h>
 /**
  * *_strcpy - Prints a string in reverse
- * @*dest: The string to print
- * @*src: sqdjfq
+ * @dest: The string to print
+ * @src: sqdjfq
  * Return: void
  */
 char *_strcpy(char *dest, char *src)
+{
+	src = dest;
+	return (dest);
+}
