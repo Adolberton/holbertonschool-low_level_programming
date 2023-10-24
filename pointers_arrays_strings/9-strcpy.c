57e2baa67c66bf633a03506d62c220b523d14e1d
Adolberton <7557@holbertonstudents.com>
2023-10-24 04:26:29 -0700

    auto commit

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+        char *start = dest;
 
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i + 1] = '\0';
-	return (dest);
+        while (*src != '\0')
+        {
+                *dest = *src;
+                dest++;
+                src++;
+        }
+        *dest = '\0';
+        return (start);
 }
