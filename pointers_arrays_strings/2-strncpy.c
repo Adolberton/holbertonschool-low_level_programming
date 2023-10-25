83f536e2fc43b4ee1151b13da49279cb197f4ee2
Adolberton <7557@holbertonstudents.com>
2023-10-25 04:56:38 -0700

    auto commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 	dest[i] = src[i];
 	i++;
-	if (i == n)
-		break;
 	}
-dest[i] = '\0';
-return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
