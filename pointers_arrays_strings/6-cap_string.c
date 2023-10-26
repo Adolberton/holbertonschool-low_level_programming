87fed5618dfa2f701c3b517fe4bd7ebe3df80cd9
Adolberton <7557@holbertonstudents.com>
2023-10-26 06:46:31 -0700

    auto commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,16 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] > 96 && str[i] < 123)
-		str[i] -= 32;
-		i++;
+		if ((str[i] > 96 && str[i] < 123) &&
+				(str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
+				 str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
+				 str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
+				 str[i - 1] == 33 || str[i - 1] == 40 || str[i - 1] == 41 ||
+				 str[i - 1] == 123 || str[i - 1] == 125))
+
+			str[i] -= 32;
 	}
+	i++;
 	return (str);
 }
 
