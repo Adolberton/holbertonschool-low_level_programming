b64e001c183b1f1c33718910fd5fe2a2d10b42b5
Adolberton <7557@holbertonstudents.com>
2023-10-27 00:37:09 -0700

    auto commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,18 +8,25 @@
 char *cap_string(char *str)
 
 {
+	int j = 0;
 	int i = 0;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+			'"', '(', ')', '{', '}'};
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if ((str[i] > 96 && str[i] < 123) &&
-				(str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
-				 str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
-				 str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
-				 str[i - 1] == 33 || str[i - 1] == 40 || str[i - 1] == 41 ||
-				 str[i - 1] == 123 || str[i - 1] == 125))
-
-			str[i] -= 32;
+		while (sep[j])
+		{
+			if (i == 0)
+			{
+				str[i] -= 32;
+			}
+			if (str[i] == sep[j])
+			{
+				str[i + 1] -= 32;
+			}
+			j++;
+		}
 		i++;
 	}
 	return (str);
