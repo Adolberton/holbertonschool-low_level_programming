14b174fa59ae9caaa6c2fb177ca154a5fe52a9a9
Adolberton <7557@holbertonstudents.com>
2023-11-01 10:10:59 -0700

    auto commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,35 @@
 #include "main.h"
 /**
- * is_prime_number - Check if a given integer is a prime number.
- * @n: The integer to be checked for primality.
+ * is_prime_recursive - Check if a number is prime using recursion.
+ * @n: The number to be checked.
+ * @divisor: The current divisor being tested.
  *
- * Return: 1 if 'n' is a prime number, 0 otherwise.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_prime_recursive(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0); // 0 and 1 are not prime numbers.
+	}
+	if (divisor == 1)
+	{
+		return (1); // 2 is a prime number, and n is not divisible by 1.
+	}
+	if (n % divisor == 0)
+	{
+		return (0); // n is divisible by divisor, not a prime number.
+	}
+	return (is_prime_recursive(n, divisor - 1));
+}
+
+/**
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-
-			return (0);
-		}
-
-	return (1);
+	return (is_prime_recursive(n, n - 1));
 }
-
-
---
+++ recursion/test.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * is_prime_number - Check if a given integer is a prime number.
+ * @n: The integer to be checked for primality.
+ *
+ * Return: 1 if 'n' is a prime number, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	if (i * i <= n && 
+	
