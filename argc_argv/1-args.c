b1efd0cd88d019cb946a39d86ba6fad81f96e220
Adolberton <7557@holbertonstudents.com>
2023-11-02 07:17:55 -0700

    auto commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the program's arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+	printf("%d\n", i);
+	return (0);
+}
