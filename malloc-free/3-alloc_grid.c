76daf6a2f76e2101e807a0e13bd8fed1866b75a8
Adolberton <7557@holbertonstudents.com>
2023-11-07 10:23:16 -0800

    auto commit

---
+++ malloc-free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Concatenates two strings
+ * @width: mabite
+ * @height: sqdfsq
+ * Return: returns the concatenated string
+ */
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	int i, j;
+	int **fk = malloc(height * sizeof(int *));
+
+	if (fk == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		fk[i] = malloc(width * sizeof(int));
+
+		if (fk[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			fk[i][j] = 0;
+		}
+	}
+
+	return (fk);
+}
