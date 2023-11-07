9dd4e5a05c9019771867dc79719d775563816dc6
Adolberton <7557@holbertonstudents.com>
2023-11-07 02:27:10 -0800

    auto commit

--- malloc-free/0-create_array.c
+++ malloc-free/0-create_array.c
@@ -1,25 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * create_array - creates an array of chars,
- * and initializes it with a specific char
- * @size: size of the array
- * @c: char to initialize the array with
- * Return: pointer to the array, or NULL if it fails
+ * create_array - Entry point
+ *@size: size of the array
+ *@c: character
+ * Return: a pointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
+	int i;
+	char *array = NULL;
 
 	if (size == 0)
-		return NULL;
-
-	arr = malloc(sizeof(char) * size);
-
-	if (arr == NULL)
-		return NULL;
-
-	for (unsigned int i = 0; i < size; i++)
-		arr[i] = c;
-
-	return arr;
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(char) * size);
+	for (i = 0 ; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
 }
-
---
+++ malloc-free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ * @str : mabit
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *cp = NULL;
+
+	if (str = NULL)
+	{
+	return (NULL);
+	}
+	cp = malloc(sizeof(str))
+		
+}
+
