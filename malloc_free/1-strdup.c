e6737aada754787110605ecc568a84d3331e0d10
Adolberton <7557@holbertonstudents.com>
2023-11-08 02:42:06 -0800

    auto commit

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,43 +3,34 @@
 #include "main.h"
 
 /**
- * str_concat - Concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: returns the concatenated string
+ * _strdup - Copy a string to a newly allocated space in memory
+ * @str: the string to be copied
+ * Return: returns the copy of the string
  */
 
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	int i = 0;
-	char *strced;
-	int len1 = 0;
-	int len2 = 0;
+	int i;
+	char *stred;
+	int len = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	while (s1[len1] != '\0')
+	while (str[len] != '\0')
 	{
-		len1++;
+		len++;
 	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	strced = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (strced == NULL)
+	stred = malloc(sizeof(char) * (len + 1));
+	if (stred == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0 ; i < len1 ; i++)
+	for (i = 0 ; i < len ; i++)
 	{
-		strced[i] = s1[i];
+		stred[i] = str[i];
 	}
-	for (i = 0 ; i < len2 ; i++)
-	{
-	strced[len1 + i] = s2[i];
-	}
-	return (strced);
+	stred[i] = '\0';
+	return (stred);
 }
