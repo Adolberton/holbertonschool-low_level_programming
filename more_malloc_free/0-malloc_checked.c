28cbdc5149dbb55c0e878f2ea3906602048def2e
Adolberton <7557@holbertonstudents.com>
2023-11-09 00:53:11 -0800

    auto commit

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,21 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - Alloue de la mémoire et vérifie l'allocation.
+ * @b: Taille de la mémoire à allouer.
+ *
+ * Return: Un pointeur vers la mémoire allouée.
+ *         Si l'allocation échoue, termine le programme avec le code d'erreur 98.
+ */
 void *malloc_checked(unsigned int b)
 {
-	*ptr = malloc(sizeof(b));
+	void *ptr;
+
+	ptr = malloc(b);
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	return(ptr);
+	return (ptr);
 }
