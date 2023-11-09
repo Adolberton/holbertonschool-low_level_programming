3792f38ccf5666eaead1c2431af6ecf0b70e954a
Adolberton <7557@holbertonstudents.com>
2023-11-09 10:22:51 -0800

    auto commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,22 +10,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i = 0;
-	int *ptr;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-	{
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
 		return (NULL);
-	}
-	while (i < nmemb)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return (ptr);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
 }
