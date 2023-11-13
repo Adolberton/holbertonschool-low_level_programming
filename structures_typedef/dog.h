af5493819c90ae6609407eae60baded8a460d796
Adolberton <7557@holbertonstudents.com>
2023-11-13 09:32:19 -0800

    auto commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,10 +7,6 @@
  * @age: age of the dog
  * @owner: owner of the dog
 */
-typedef struct dog dog_t;
-/**
- * dog_t - Typedef for struct dog
- */
 struct dog
 {
 	char *name;
@@ -20,6 +16,10 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
