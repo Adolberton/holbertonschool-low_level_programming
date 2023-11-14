bf92d82f7f2fd878cb55c79d92b4db0a911ac436
Adolberton <7557@holbertonstudents.com>
2023-11-14 00:31:20 -0800

    auto commit

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,11 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
-	while (i < size)
+	if (array && action)
 	{
-		action(array[i]);
-		i++;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
-
-
