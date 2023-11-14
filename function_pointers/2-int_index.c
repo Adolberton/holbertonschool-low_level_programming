9e06f47428c639eee5f75a039d7ab2d32c2326eb
Adolberton <7557@holbertonstudents.com>
2023-11-14 00:56:34 -0800

    auto commit

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,18 +13,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	while (i < size)
+
+	for (i = 0; i < size; i++)
 	{
-		cmp (array[i]);
-		if (cmp (array[i]) != 0)
+		if (cmp(array[i]) != 0)
 			return (i);
-		i++;
 	}
+
 	return (-1);
 }
-
-
