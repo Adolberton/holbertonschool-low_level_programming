cb992c50b113353bd033af245e818208553fb7fe
Adolberton <7557@holbertonstudents.com>
2023-11-17 02:51:20 -0800

    auto commit

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Calcule la somme de ses paramètres.
+ * @n: Le nombre de paramètres.
+ * @separator: Les paramètres à additionner.
+ *
+ * Return: La somme des paramètres.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+}
