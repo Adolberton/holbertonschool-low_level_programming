c68db77dece63e72bbd425fc40a2a5da8002529b
root <root@DESKTOP-1V1HPI1>
2023-11-27 14:06:12 +0100

    auto commit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h);
+{
+	size_t teubi;
+
+	teubi = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		teubi++;
+	}
+	return (teubi);
+}
