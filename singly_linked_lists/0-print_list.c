0298b2b9f429b111e881a0e0bf1501bad9742a06
Adolberton <7557@holbertonstudents.com>
2023-11-27 14:41:50 +0100

    auto commit

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+
 size_t print_list(const list_t *h);
 {
 	size_t teubi;
