b417103e8ab80290fe3a2c41920ed65d8e2ae04a
Adolberton <7557@holbertonstudents.com>
2023-11-29 10:42:54 +0100

    autocommit

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,21 +1,26 @@
 #include "lists.h"
-
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to be added to the list
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->next = *head;
