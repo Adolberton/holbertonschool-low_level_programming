66d672eb0cbe84261dd1fab8c597225521a2f3a7
Adolberton <7557@holbertonstudents.com>
2023-11-29 14:12:39 +0100

    autocommit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,8 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t i = 0;
 
 	while (h != NULL)
-		h = h->prev;
-	while (h != NULL)
 	{
-	printf("%d", h->n);
+	printf("%d\n", h->n);
 	h = h->next;
 	i++;
 	}
