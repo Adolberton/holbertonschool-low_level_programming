45bd5ec89db8060db3164b7eb54178e9817dcfb2
Adolberton <7557@holbertonstudents.com>
2023-12-04 16:07:50 +0100

    autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
@@ -29,7 +29,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-
 		w = write(fd, text_content, i);
 
 		if (w == -1)
