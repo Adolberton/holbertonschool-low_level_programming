19df500f40e035e744d56da7839da9581fd413cc
Adolberton <7557@holbertonstudents.com>
2023-12-04 15:55:25 +0100

    autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,8 +27,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[len])
 			len++;
+			write(fd, text_content, len);
 	}
-	write(fd, text_content, len);
 	close(fd);
 	return (1);
 }
