21ac8ccdb25d5762c892bb371612ccb97cc29733
Adolberton <7557@holbertonstudents.com>
2023-12-04 15:26:32 +0100

    autocommit

--- file_io/main.h
+++ file_io/main.h
@@ -1,17 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 
-
 ssize_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif
