d484d005a083e07d5486f5314f536782fc0abecf
Adolberton <7557@holbertonstudents.com>
2023-12-04 15:56:14 +0100

    autocommit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,22 +13,30 @@
  * if text_content is NULL create an empty file
  */
 int create_file(const char *filename, char *text_content)
-{
-	int fd = 0, len;
 
-	if (!filename)
+{
+	int fd, i, w;
+
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
-		while (text_content[len])
-			len++;
-			write(fd, text_content, len);
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+
+		w = write(fd, text_content, i);
+
+		if (w == -1)
+			return (-1);
 	}
+
 	close(fd);
+
 	return (1);
 }
