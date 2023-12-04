16a113ebdbd85b82abcc670d393d359cb002b36f
Adolberton <7557@holbertonstudents.com>
2023-12-04 15:53:51 +0100

    autocommit

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ * File cannot be created, file cannot be written, write “fails”, etc…
+ * The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
