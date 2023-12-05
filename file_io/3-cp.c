2389d906cdbf29f3e695e28d410033af2e37c224
Adolberton <7557@holbertonstudents.com>
2023-12-05 10:58:00 +0100

    autocommit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",argv[1]),
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
