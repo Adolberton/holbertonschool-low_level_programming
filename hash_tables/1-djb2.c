df07af5c45a5c402a8f101ae7a62f8aa1fe026cc
Adolberton <7557@holbertonstudents.com>
2023-12-07 13:21:38 +0100

    autocommit

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string to hash
+ *
+ * Return: hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str);
+
+
